6a8ee930b7648d12aa8ae5e15478c1eee7cf6c07
Nilso Jr <nilso.cjr@gmail.com>
2022-01-06 21:26:19 -0300

    chore: added lints, styles and typescript configs

---
+++ client/pages/api/hello.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+export default (req: NextApiRequest, res: NextApiResponse<Data>): void => {
+  res.status(200).json({ name: "John Doe" });
+};
