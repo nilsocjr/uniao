5b7c2c8587c7c49f8a3051daecc180c8e7ca0954
Nilso Jr <nilso.cjr@gmail.com>
2022-01-19 21:43:35 -0300

    feat: added libs for seo

---
+++ client/public/sitemap-generator.js
@@ -0,0 +1,25 @@
+const sitemapGenetatorConfig = {
+  siteUrl: 'https://retificauniao.com.br',
+  generateRobotsTxt: true,
+  exclude: ['/pt-br*', '/disallowed'],
+  alternateRefs: [
+    {
+      href: 'https://retificauniao.com.br/pt-br',
+      hreflang: 'pt-br',
+    },
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: '/disallowed',
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+}
+
+module.exports = sitemapGenetatorConfig
